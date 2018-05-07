8d5bcd72cced3dd50ad9ca59180d1a0eee8b20d8
Amanda Prorok <amanda.prorok@gmail.com>
2018-05-07 18:41:28 +0100

    .

--- plot_losses.py
+++ plot_losses.py
@@ -5,23 +5,27 @@ from __future__ import print_function
 import argparse
 import json
 import matplotlib.pylab as plt
+import os
 
 
 def main(args):
-  with open(args.log_file, 'r') as fp:
-    data = json.load(fp)
+  for filename in args.log_file.split(','):
+    with open(filename, 'r') as fp:
+      data = json.load(fp)
+    label = os.path.basename(filename)
+    epochs = []
+    train_abs = []
+    valid_abs = []
+    for d in data:
+      epochs.append(d['epoch'])
+      train_abs.append(d['train_results'][1][0])
+      valid_abs.append(d['valid_results'][1][0])
+    if args.plot_train:
+      plt.plot(epochs, train_abs, label=label + ' (train)')
+    plt.plot(epochs, valid_abs, label=label)
 
-  epochs = []
-  train_abs = []
-  valid_abs = []
-  for d in data:
-    epochs.append(d['epoch'])
-    train_abs.append(d['train_results'][1][0])
-    valid_abs.append(d['valid_results'][1][0])
-  plt.plot(epochs, train_abs, label='Training')
-  plt.plot(epochs, valid_abs, label='Evaluation')
   plt.xlabel('Epoch')
-  plt.ylabel('Accucary')
+  plt.ylabel('L1 loss')
   plt.legend()
   plt.show()
 
@@ -29,5 +33,6 @@ def main(args):
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Generates random graph')
   parser.add_argument('--log_file', action='store', required=True, help='Path where the json file is stored')
+  parser.add_argument('--plot_train', action='store_true', help='Plots the training loss too')
   args = parser.parse_args()
   main(args)
